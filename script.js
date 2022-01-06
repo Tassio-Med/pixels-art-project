afeed4b64e98367741691078aabfdf449ce3f80e
Tassio Med <tassiohfmed@gmail.com>
2022-01-05 23:29:11 -0300

    criando os botoes

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let grid = 5;
+let btnBlack =  document.querySelector(".preto");
+let btnOrange = document.querySelector(".laranja");
+let btnPink = document.querySelector(".rosa");
+let btnPurple = document.querySelector(".roxo");
 
 for(let i = 0; i < grid; i += 1) {
   let lines = document.createElement('div');
   lines.classList.add('linhas');
   document.getElementById('pixel-board').appendChild(lines);
   
-  for(let index = 0; index < grid; index +=1) {       /* Resolvido com ajuda do do Gean Antunes, da Turma 19-B */ 
+  for(let index = 0; index < grid; index += 1) {       /* Resolvido com ajuda do do Gean Antunes, da Turma 19-B */ 
     let column = document.createElement('div');
     column.classList.add('pixel');
     lines.appendChild(column);
