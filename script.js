998859282d268d66dac16064479e112baddf4705
Tassio Med <tassiohfmed@gmail.com>
2022-01-05 22:33:00 -0300

    adicionando ajuda do Gean

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ for(let i = 0; i < grid; i += 1) {
   lines.classList.add('linhas');
   document.getElementById('pixel-board').appendChild(lines);
   
-  for(let index = 0; index < grid; index +=1) {
+  for(let index = 0; index < grid; index +=1) {       /* Resolvido com ajuda do do Gean Antunes, da Turma 19-B */ 
     let column = document.createElement('div');
     column.classList.add('pixel');
     lines.appendChild(column);
