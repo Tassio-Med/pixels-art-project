5f634c795cdf331b004c49f118bcd685cad803ec
Tassio Med <tassiohfmed@gmail.com>
2022-01-08 13:16:05 -0300

    8 requisitos completos

--- script.js
+++ script.js
@@ -1,37 +1,33 @@
 let grid = 5;
 
-
 for(let i = 0; i < grid; i += 1) {
-  let lines = document.createElement('div');
-  lines.classList.add('linhas');
+  let lines = document.createElement('div');   lines.classList.add('linhas');
   document.getElementById('pixel-board').appendChild(lines);
-  
-  for(let index = 0; index < grid; index += 1) {       /* Resolvido com ajuda do do Gean Antunes, da Turma 19-B */ 
-    let lines = document.createElement('div');
-    lines.classList.add('pixel');
-    document.getElementById('pixel-board').appendChild(lines);
+    for(let index = 0; index < grid; index += 1) {       /* Resolvido com ajuda do do Gean Antunes, da Turma 19-B */ 
+      let lines = document.createElement('div');
+      lines.classList.add('pixel');
+      document.getElementById('pixel-board').appendChild(lines);
   
   }
 }
+
 
 /* Mudar classe 'selected' */
 
 let palletColor = document.querySelector('#color-palette');
 let colorSelected = document.querySelector('.selected');
 
-function changeClass(event) {
+function changeClass(event) {          /* ---> Resolvido com a ajuda do Kael caldeira - Turma 19-B */
   colorSelected = document.querySelector('.selected');
-  colorSelected.className = 'color';
-
-  event.target.className = 'color selected';
+  colorSelected.classList.remove('selected');
+  event.target.classList.add('selected');
 }
-
   palletColor.addEventListener('click', changeClass);  
 
 
 /* Mudar a cor do pixel */
 
-function changeColor(event) {
+function changeColor() {
   let selectedClass = document.querySelector('.selected');  /* --> /* Resolvido com ajuda do do Gean Antunes, da Turma 19-B */  
   event.target.style.backgroundColor = window.getComputedStyle(selectedClass).getPropertyValue('background-color');
 }
