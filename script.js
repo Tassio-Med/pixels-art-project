d0c92861387dcc9e5ff0e0f4082d25cc811d2eca
Tassio Med <tassiohfmed@gmail.com>
2022-01-08 11:34:14 -0300

    faltando apenas o requisito 8

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 let grid = 5;
-/* let btnBlack =  document.querySelector(".preto");
-  let btnOrange = document.querySelector(".laranja");
-  let btnPink = document.querySelector(".rosa");
-  let btnPurple = document.querySelector(".roxo"); */
+
 
 for(let i = 0; i < grid; i += 1) {
   let lines = document.createElement('div');
@@ -10,9 +7,10 @@ for(let i = 0; i < grid; i += 1) {
   document.getElementById('pixel-board').appendChild(lines);
   
   for(let index = 0; index < grid; index += 1) {       /* Resolvido com ajuda do do Gean Antunes, da Turma 19-B */ 
-    let column = document.createElement('div');
-    column.classList.add('pixel');
-    lines.appendChild(column);
+    let lines = document.createElement('div');
+    lines.classList.add('pixel');
+    document.getElementById('pixel-board').appendChild(lines);
+  
   }
 }
 
@@ -29,3 +27,12 @@ function changeClass(event) {
 }
 
   palletColor.addEventListener('click', changeClass);  
+
+
+/* Mudar a cor do pixel */
+
+function changeColor(event) {
+  let selectedClass = document.querySelector('.selected');  /* --> /* Resolvido com ajuda do do Gean Antunes, da Turma 19-B */  
+  event.target.style.backgroundColor = window.getComputedStyle(selectedClass).getPropertyValue('background-color');
+}
+document.querySelector('#pixel-board').addEventListener('click', changeColor);
